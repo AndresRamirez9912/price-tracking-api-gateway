105f5947255dadd0adc2db5fd2365b2f3baf01e2
Andres Ramirez <andres.ramirez9912@gmail.com>
2024-03-18 09:44:08 -0500

    Update dockerfile for Pi

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module price-tracking-api-gateway
 
 go 1.20
 
-require (
-	github.com/go-chi/chi/v5 v5.0.11
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/go-chi/chi/v5 v5.0.11
--- main.go
+++ main.go
@@ -9,17 +9,9 @@ import (
 	"price-tracking-api-gateway/src/middlewares"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Read the .env variables
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Println("Error loading the .env variables", err)
-		return
-	}
-
 	// Main Router
 	r := chi.NewRouter()
 	auth := r.Group(nil)
@@ -31,7 +23,7 @@ func main() {
 	auth.Post("/api/*", handlers.ForwardingV1)
 
 	log.Println("Serving API Gateway on port", os.Getenv(constants.PORT))
-	err = http.ListenAndServe(os.Getenv(constants.PORT), r)
+	err := http.ListenAndServe(os.Getenv(constants.PORT), r)
 	if err != nil {
 		log.Fatal("Error serving the application", err)
 	}
